eeaffea221c6868edf8d53deb2dae9d072afb38b
Kevin Fernández <juankfernandez@protonmail.com>
2020-05-16 21:42:55 +0200

    master

--- ing/build/all/app/CMakeFiles/ing.desktop.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ing.desktop"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ing.desktop.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
